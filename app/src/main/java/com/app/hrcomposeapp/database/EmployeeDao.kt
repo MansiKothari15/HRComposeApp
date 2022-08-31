4e939b8c5408fa3df2609d13d89b85e2d9ca20c0
Mansi <mansi.kothari@bacancy.com>
2022-08-31 17:04:19 +0530

    made suspend function in DAO added volatile in DB

--- app/src/main/java/com/app/hrcomposeapp/database/EmployeeDao.kt
+++ app/src/main/java/com/app/hrcomposeapp/database/EmployeeDao.kt
@@ -5,18 +5,18 @@ import androidx.room.*
 @Dao
 interface EmployeeDao {
 
-    @Insert
-    fun addEmployee(employee: Employee)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addEmployee(employee: Employee)
 
     @Query("SELECT * FROM employees WHERE employeeId = :empId")
     fun findEmployeeById(empId: String): Employee
 
     @Query("SELECT * FROM employees")
-    suspend fun getAllEmployees(): List<Employee>
+    fun getAllEmployees(): List<Employee>
 
     @Update
-    fun updateEmployeeDetails(employee: Employee)
+    suspend fun updateEmployeeDetails(employee: Employee)
 
     @Delete
-    fun deleteEmployee(employee: Employee)
+    suspend fun deleteEmployee(employee: Employee)
 }
--- app/src/main/java/com/app/hrcomposeapp/database/EmployeeRoomDatabase.kt
+++ app/src/main/java/com/app/hrcomposeapp/database/EmployeeRoomDatabase.kt
@@ -5,15 +5,20 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [(Employee::class)], version = 1)
+@Database(entities = [(Employee::class)], version = 1, exportSchema = false)
 abstract class EmployeeRoomDatabase : RoomDatabase() {
 
     abstract fun employeeDao(): EmployeeDao
 
     companion object {
+        /*The value of a volatile variable will never be cached, and all writes and reads will be done to and from the main memory.
+        This helps make sure the value of INSTANCE is always up-to-date and the same for all execution threads.
+        It means that changes made by one thread to INSTANCE are visible to all other threads immediately.*/
+        @Volatile
         private var INSTANCE: EmployeeRoomDatabase? = null
 
         fun getInstance(context: Context): EmployeeRoomDatabase {
+            // only one thread of execution at a time can enter this block of code
             synchronized(this) {
                 var instance = INSTANCE
 
