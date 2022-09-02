0a3a0d395ba339e38c6a396e5fdf1cc0a1c513d1
DharmeshBasapatiBacancy <dharmesh.basapati@bacancy.com>
2022-09-02 18:14:49 +0530

    Updated exciting colors.

--- app/src/main/java/com/app/hrcomposeapp/ui/theme/Color.kt
+++ app/src/main/java/com/app/hrcomposeapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.app.hrcomposeapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val PrimaryColor = Color(0xFF234e70)
-val PrimaryDarkColor = Color(0xFF002645)
-val ColorOnPrimary = Color(0xFFFFFFFF)
+val PrimaryColor = Color(0xFF1dc391)
+val PrimaryDarkColor = Color(0xFF009163)
+val ColorOnPrimary = Color(0xFF000000)
 
-val SecondaryColor = Color(0xFFfbf8be)
-val SecondaryDarkColor = Color(0xFFc7c58d)
+val SecondaryColor = Color(0xFFf8ca41)
+val SecondaryDarkColor = Color(0xFFc19a00)
 val ColorOnSecondary = Color(0xFF000000)
